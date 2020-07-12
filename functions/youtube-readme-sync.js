be956292999f7ada33445037b3c136cec7e73194
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-07-12 11:13:26 +0200

    Update

--- functions/youtube-readme-sync.js
+++ functions/youtube-readme-sync.js
@@ -1,6 +1,6 @@
-const axios = require("axios").default
-const queryString = require("query-string")
-const github = require("octonode")
+const axios = require('axios').default
+const queryString = require('query-string')
+const github = require('octonode')
 
 const ghClient = github.client({
     username: process.env.GITHUB_USERNAME,
@@ -10,63 +10,46 @@ const ghClient = github.client({
 const ghRepo = ghClient.repo(process.env.GITHUB_REPO)
 
 const YT_SEARCH_PARAMS = {
-    part: "snippet",
+    part: 'snippet',
     channelId: process.env.YOUTUBE_CHANNEL_ID,
     maxResults: 3,
-    order: "date",
-    type: "video",
+    order: 'date',
+    type: 'video',
     key: process.env.GOOGLE_API_KEY,
 }
 
-const START_COMMENT = "<!-- YT TABLE START -->"
-const END_COMMENT = "<!-- YT TABLE END -->"
+const START_COMMENT = '<!-- YT LIST START -->'
+const END_COMMENT = '<!-- YT LIST END -->'
 
 const run = async () => {
     // Get the latest YouTube videos for your channel
     const response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/search?${queryString.stringify(
-            YT_SEARCH_PARAMS
-        )}`
+        `https://www.googleapis.com/youtube/v3/search?${queryString.stringify(YT_SEARCH_PARAMS)}`,
     )
     const latestVideos = response.data.items
 
+    const divider = `\n<img align="center" width="100%" height="0" />\n`
+
     // Generate the table
-    const imageRow = latestVideos.map((video) => {
-        return `<a href="https://www.youtube.com/watch?v=${video.id.videoId}"><img src="https://img.youtube.com/vi/${video.id.videoId}/maxresdefault.jpg" width="200" style="border-radius:20px;" /></a>`
+    const videoRows = latestVideos.map((video) => {
+        const id = video.id.videoId
+        const title = video.snippet.title.split('|')[0]
+
+        // prettier-ignore
+        return `[<img src="https://img.youtube.com/vi/${id}/maxresdefault.jpg" align="left" width="200" />](https://www.youtube.com/watch?v=${id}) **[${title}](https://www.youtube.com/watch?v=${id})**`
     })
 
-    const dividerRow = latestVideos.map(() => "---")
-
-    const titleRow = latestVideos.map((video) => {
-        return `**[${
-            video.snippet.title.split("|")[0]
-        }](https://www.youtube.com/watch?v=${video.id.videoId})**`
-    })
-
-    const rows = [imageRow, dividerRow, titleRow].map(
-        (row) => `| ${row.join(" | ")} |`
-    )
-    const table = `${START_COMMENT}\n${rows.join("\n")}\n${END_COMMENT}`
+    const newList = `${START_COMMENT}\n${videoRows.join(divider)}\n${END_COMMENT}`
 
     // Get the current README.md
     const [readme] = await ghRepo.readmeAsync()
-    const readmeContent = Buffer.from(readme.content, "base64").toString(
-        "utf-8"
-    )
+    const readmeContent = Buffer.from(readme.content, 'base64').toString('utf-8')
 
     // Replace the existing videos table with the new one
-    const newReadmeContent =
-        readmeContent.split(START_COMMENT)[0] +
-        table +
-        readmeContent.split(END_COMMENT)[1]
+    const newReadmeContent = readmeContent.split(START_COMMENT)[0] + newList + readmeContent.split(END_COMMENT)[1]
 
     // Update the README.md
-    await ghRepo.updateContentsAsync(
-        readme.path,
-        "Update videos table",
-        newReadmeContent,
-        readme.sha
-    )
+    await ghRepo.updateContentsAsync(readme.path, 'Update videos table', newReadmeContent, readme.sha)
 }
 
 // The Netlify Functions handler function
@@ -78,13 +61,13 @@ exports.handler = async function () {
 
         return {
             statusCode: 500,
-            body: "Internal Server Error",
+            body: 'Internal Server Error',
         }
     }
 
     return {
         statusCode: 200,
-        body: "OK",
+        body: 'OK',
     }
 }
 
